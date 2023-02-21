724fb1747d9d3b9aa049bb6271efae4c6a2a322b
Marcin Miszewski <marcin.miszewsky@gmail.com>
2023-02-21 15:13:51 +0100

    Add AppBar and TextFields

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,11 +16,60 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       home: Scaffold(
+        backgroundColor: Colors.transparent,
+        appBar: AppBar(
+          title: Center(
+            child: Text(
+              'MOBILE GARAGE',
+              style: GoogleFonts.tourney(
+                textStyle: const TextStyle(
+                  fontSize: 40,
+                  color: Colors.amber,
+                ),
+              ),
+            ),
+          ),
+          backgroundColor: Colors.transparent,
+          elevation: 0.0,
+        ),
         body: Container(
           decoration: const BoxDecoration(
             image: DecorationImage(
               image: AssetImage('images/garage.jpg'),
               fit: BoxFit.cover,
+            ),
+          ),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  decoration: const BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(10),
+                    ),
+                  ),
+                  padding: const EdgeInsets.only(left: 10),
+                  margin: const EdgeInsets.all(10),
+                  child: const TextField(
+                    decoration: InputDecoration(hintText: 'E-mail'),
+                  ),
+                ),
+                Container(
+                  decoration: const BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(10),
+                    ),
+                  ),
+                  padding: const EdgeInsets.only(left: 10),
+                  margin: const EdgeInsets.all(10),
+                  child: const TextField(
+                    decoration: InputDecoration(hintText: 'Password'),
+                  ),
+                )
+              ],
             ),
           ),
         ),
