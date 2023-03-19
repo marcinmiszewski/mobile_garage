4a470f4fe63e0f0be618766b47f20fc34c649c30
Marcin Miszewski <marcin.miszewsky@gmail.com>
2023-03-19 20:12:01 +0100

    Install FirebaseAuth

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -22,69 +23,102 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Scaffold(
-        backgroundColor: Colors.transparent,
-        appBar: AppBar(
-          title: Center(
-            child: Text(
-              'MOBILE GARAGE',
-              style: GoogleFonts.tourney(
-                textStyle: const TextStyle(
-                  fontSize: 40,
-                  color: Colors.amber,
+      home: const RootPage(),
+    );
+  }
+}
+
+class RootPage extends StatelessWidget {
+  const RootPage({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<User?>(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, snapshot) {
+          final user = snapshot.data;
+          if (user == null) {
+            return Scaffold(
+              backgroundColor: Colors.transparent,
+              appBar: AppBar(
+                title: Center(
+                  child: Text(
+                    'MOBILE GARAGE',
+                    style: GoogleFonts.tourney(
+                      textStyle: const TextStyle(
+                        fontSize: 40,
+                        color: Colors.amber,
+                      ),
+                    ),
+                  ),
+                ),
+                backgroundColor: Colors.transparent,
+                elevation: 0.0,
+              ),
+              body: Container(
+                decoration: const BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage('images/garage.jpg'),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                child: Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        'Jesteś niezalogowany',
+                        style: GoogleFonts.lobster(
+                          color: Colors.amber,
+                          fontSize: 20,
+                          letterSpacing: 3,
+                        ),
+                      ),
+                      Container(
+                        decoration: const BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(10),
+                          ),
+                        ),
+                        padding: const EdgeInsets.only(
+                            left: 10, top: 13, bottom: 13),
+                        margin: const EdgeInsets.all(10),
+                        child: const TextField(
+                          decoration: InputDecoration.collapsed(
+                            hintText: 'E-mail',
+                          ),
+                        ),
+                      ),
+                      Container(
+                        decoration: const BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(10),
+                          ),
+                        ),
+                        padding: const EdgeInsets.only(
+                            left: 10, top: 13, bottom: 13),
+                        margin: const EdgeInsets.all(10),
+                        child: const TextField(
+                          decoration: InputDecoration.collapsed(
+                            hintText: 'Password',
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
                 ),
               ),
+            );
+          }
+          return Scaffold(
+            body: Center(
+              child: Text('Jesteś zalogowany jako ${user.email}'),
             ),
-          ),
-          backgroundColor: Colors.transparent,
-          elevation: 0.0,
-        ),
-        body: Container(
-          decoration: const BoxDecoration(
-            image: DecorationImage(
-              image: AssetImage('images/garage.jpg'),
-              fit: BoxFit.cover,
-            ),
-          ),
-          child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Container(
-                  decoration: const BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.all(
-                      Radius.circular(10),
-                    ),
-                  ),
-                  padding: const EdgeInsets.only(left: 10, top: 13, bottom: 13),
-                  margin: const EdgeInsets.all(10),
-                  child: const TextField(
-                    decoration: InputDecoration.collapsed(
-                      hintText: 'E-mail',
-                    ),
-                  ),
-                ),
-                Container(
-                  decoration: const BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.all(
-                      Radius.circular(10),
-                    ),
-                  ),
-                  padding: const EdgeInsets.only(left: 10, top: 13, bottom: 13),
-                  margin: const EdgeInsets.all(10),
-                  child: const TextField(
-                    decoration: InputDecoration.collapsed(
-                      hintText: 'Password',
-                    ),
-                  ),
-                )
-              ],
-            ),
-          ),
-        ),
-      ),
-    );
+          );
+        });
   }
 }
