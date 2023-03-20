754c12b865d0045d2c4897d2e6f1661db556ad79
Marcin Miszewski <marcin.miszewsky@gmail.com>
2023-03-20 19:37:01 +0100

    Add BottomNavigationBar to HomePage

--- lib/app/home/home_page.dart
+++ lib/app/home/home_page.dart
@@ -1,7 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({
     super.key,
     required this.user,
@@ -10,26 +10,71 @@ class HomePage extends StatelessWidget {
   final User user;
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  var currentIndex = 0;
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              'Jesteś zalogowany jako ${user.email}',
+      body: Builder(builder: (context) {
+        if (currentIndex == 0) {
+          return Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'Jesteś zalogowany jako ${widget.user.email}',
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    FirebaseAuth.instance.signOut();
+                  },
+                  child: const Text('Wyloguj'),
+                ),
+              ],
             ),
-            const SizedBox(
-              height: 20,
+          );
+        }
+        if (currentIndex == 1) {
+          return const Center(
+            child: Text(
+              'Dodaj wydatek',
             ),
-            ElevatedButton(
-              onPressed: () {
-                FirebaseAuth.instance.signOut();
-              },
-              child: const Text('Wyloguj'),
-            ),
-          ],
-        ),
+          );
+        }
+        return const Center(
+          child: Text(
+            'Wydatki',
+          ),
+        );
+      }),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: currentIndex,
+        onTap: (newIndex) {
+          setState(() {
+            currentIndex = newIndex;
+          });
+        },
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Moje konto',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.add),
+            label: 'Dodaj wydatek',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.currency_exchange),
+            label: 'Wydatki',
+          ),
+        ],
       ),
     );
   }
