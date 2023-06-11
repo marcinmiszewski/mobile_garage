70ad354959d375eb418dc6808bff41d8d6641243
Marcin Miszewski <marcin.miszewsky@gmail.com>
2023-06-11 21:03:39 +0200

    Move MyApp to app.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,8 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:mobile_garage/main.dart';
+import 'package:mobile_garage/app/features/app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
